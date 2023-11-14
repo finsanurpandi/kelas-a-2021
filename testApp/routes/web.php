d24e11ccdbd4d1d11168037b822ddff7b6d1c539
Finsa Nurpandi <finsaworks@gmail.com>
2023-11-14 16:18:47 +0700

    seeding, authentication, authorization

--- testApp/routes/web.php
+++ testApp/routes/web.php
@@ -40,6 +40,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::group(['middleware' => ['role:admin|user']], function () {
+    Route::get('/data', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('data');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
